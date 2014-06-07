12c495000daa7ea4247b75a3f3b0cc11d7232910
ScimitarDD <davidh93@hotmail.de>
2014-06-07 16:19:13 +0200

    robins koordinaten xD

---
+++ SchiffeVersenken/src/schiffeVersenken/gruppe2/Coordinate.java
@@ -0,0 +1,34 @@
+package schiffeVersenken.gruppe2;
+
+public class Coordinate
+{
+	private int x;
+	private int y;
+	
+	public Coordinate(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Coordinate(int x, char y)
+	{
+		this.x = x;
+		this.y = Character.toUpperCase(y) - 64;
+	}
+	
+	public int getX()
+	{
+		return x;
+	}
+	
+	public int getY()
+	{
+		return y;
+	}
+	
+	public String toString()
+	{
+		return "(" + x + ", " + (char)(y + 64) + ")";
+	}
+}
