fad0e84c3f819e309473c7cfd1ff53c8dad243d7
ScimitarDD <davidh93@hotmail.de>
2014-06-08 02:19:05 +0200

    spielfeldgröße nach anforderungen

--- SchiffeVersenken/src/schiffeVersenken/gruppe2/BattlefieldConstants.java
+++ SchiffeVersenken/src/schiffeVersenken/gruppe2/BattlefieldConstants.java
@@ -9,8 +9,8 @@ package schiffeVersenken.gruppe2;
 public interface BattlefieldConstants {
 
 	//constants
-	public final int bfWidth=10;
-	public final int bfHeight=10;
+	public final int bfWidth=7;
+	public final int bfHeight=11;
 	//note: shipMaxOverlap should be 0 when shipMinDistance>0
 	public final int shipMinDistance=1;
 	public final int shipMaxOverlap=0;
