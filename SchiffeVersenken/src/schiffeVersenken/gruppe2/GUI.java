8a1e174b7fc19356bd5ad6ff6de5ccc94d1cf7e2
mathias juergens <m.juerg@online.de>
2014-06-15 22:05:30 +0200

    gui class

---
+++ SchiffeVersenken/src/schiffeVersenken/gruppe2/GUI.java
@@ -0,0 +1,18 @@
+package schiffeVersenken.gruppe2;
+
+public interface GUI {
+	
+	/**
+	 * 
+	 * @author M.Jürgens
+	 * modified by:
+	 *
+	 */
+	
+	public void print(String s);
+	public Coordinate getCoordinate(String printBefore);
+	public int getInt(String printBefore);
+	
+	
+
+}
